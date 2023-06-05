f6e55f1bf0103e5ca52c0859e40717fbba61eacc
20WH1A1221 <93463149+20WH1A1221@users.noreply.github.com>
2023-06-05 22:14:07 +0530

    updated info

---
+++ usercard2.js
@@ -0,0 +1,5 @@
+let dhonioldinfo = {
+    imgurl:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320,q_50/lsci/db/PICTURES/CMS/319900/319946.png",
+    name: "MS Dhoni",
+    discription:"Cricketer"
+} 
