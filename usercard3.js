66f2d804eaaa2bef8e8e2166d0069e11e56311d7
20WH1A1221 <93463149+20WH1A1221@users.noreply.github.com>
2023-06-13 22:37:52 +0530

    changes done

--- usercard3.js
+++ usercard3.js
@@ -8,7 +8,7 @@ let dhonioldinfo = {
 let displayobject = {
     name:"",
     imgurl:"",
-    description:""
+    discription:""
 };
 let getRandomUser = function(){
     fetch("https://randomuser.me/api")
@@ -17,7 +17,7 @@ let getRandomUser = function(){
         displayobject.name = data.results[0].name.first +" "+ data.results[0].name.last
         //displayobject.name = "tanusree"
         displayobject.imgurl = data.results[0].picture.large
-        displayobject.description = data.results[0].gender
+        displayobject.discription = data.results[0].gender
         document.getElementById("dhoni-img").src=displayobject.imgurl;
         document.getElementById("dhoni-name").innerHTML=displayobject.name;
         document.getElementById("dhoni-discription").innerHTML=displayobject.discription;
